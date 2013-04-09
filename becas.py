6efb594d34b1b263b31d2a0adf3fbe34320cf157
Tiago Nunes <tiagosbnunes@gmail.com>
2013-04-09 14:19:29 +0100

    Tag v1.0.3

--- becas.py
+++ becas.py
@@ -34,7 +34,7 @@ Resources
 '''
 
 __title__ = 'becas'
-__version__ = '1.0.2-dev'
+__version__ = '1.0.3'
 __author__ = 'Tiago Nunes'
 __license__ = 'CC-BY-NC'
 __copyright__ = 'Copyright 2013, Tiago Nunes, Universidade de Aveiro'
